2cb8f0d8073311631007d54d5432304ac815903c
huyvq63 <huyvq63@fpt.com>
2026-03-31 16:39:11 +0700

    feat: add thuha.io.vn to CORS whitelist

--- portfolio-be/src/main/java/com/couple/backend/CoupleBackendApplication.java
+++ portfolio-be/src/main/java/com/couple/backend/CoupleBackendApplication.java
@@ -34,7 +34,9 @@ public class CoupleBackendApplication {
                 "http://localhost:5173",
                 "http://localhost:5174",
                 "http://127.0.0.1:5173",
-                "http://127.0.0.1:5174"
+                "http://127.0.0.1:5174",
+                "https://www.thuha.io.vn",
+                "https://thuha.io.vn"
             );
         }
 
